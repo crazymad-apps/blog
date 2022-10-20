a3fab5eb93b6c12ea9a5c3cde0e8dc86011cc24d
crazymad-apps <crazymad0601.apps@gmail.com>
2022-10-20 20:57:40 +0800

    初始化

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <h1>crazy_mad 的个人博客</h1>
+
+  <h2>关于我</h2>
+  <article>
+    名义上我是一个前端开发，实际上也确实是一个前端开发，工作三年多技能基本都点在前端技术栈上了。
+		其实我更愿意称呼自己为程序员
+  </article>
+  <a href="/tec-blogs">tec blogs</a>
+</main>
+
+<style>
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
